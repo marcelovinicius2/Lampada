a0f36c0d4f0925495eccc4819d4e7afe0a7cf0ca
marcelovinicius2 <marcelovinicius2@hotmail.com>
2022-07-20 10:45:23 -0300

    ajuste 2

--- lampada.js
+++ lampada.js
@@ -1,20 +1,38 @@
-const turnOn = document.getElementById ("turnOn");
-const turnOff = document.getElementById ("turnoff");
+const turnOn = document.getElementById ( "turnOn" );
+const turnOff = document.getElementById ( "turnOff" );
 const lamp = document.getElementById ("lamp");
+const turnNova= document.getElementById ("turnNova");
+
+
+function islampBrokrer (){
+    return lamp.src.indexOf ( 'lampadaQuebrada') > -1
+}
 
 function lampOn(){
-    lamp.src = "./img/lampadaOn.jpg";
+    if (!islampBrokrer()){
+        lamp.src = "./img/lampadaOn.jpg";
+    }
 }
+
 function lampOff(){
+    if (!islampBrokrer()){
     lamp.src = "./img/lampadaOff.jpg";
+    }
 }
 
 function lampBroker(){
     lamp.src = "./img/lampadaQuebrada.jpg";
 }
+function lampadaNova(){
+    if (islampBrokrer()){
+    lamp.src = "./img/lampadaNova.jpg";
+}
+}
 
-turnOn.addEventListener ("click", lampOn);
-turnOn.addEventListener ("click", lampOff);
+
+turnOn.addEventListener ( "click", lampOn );
+turnOff.addEventListener ( "click", lampOff );
+turnNova.addEventListener ( "click", lampadaNova);
 lamp.addEventListener ("mouseover", lampOn);
 lamp.addEventListener ("mouseleave", lampOff);
 lamp.addEventListener ("dblclick", lampBroker);
